d257747e86925aa6466dff4563c50da66b37e11e
paulrydrick <paulrydrickpuri@gmail.com>
2025-04-15 20:58:30 +0800

    idk anymorelol

--- next.config.js
+++ next.config.js
@@ -11,5 +11,11 @@ const nextConfig = {
     },
   }
   
-  module.exports = nextConfig
-  
+  const path = require('path');
+
+  module.exports = {
+    webpack(config) {
+      config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+      return config;
+    },
+  };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,7 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Inter', 'system-ui', 'sans-serif'],
-        mono: ['Menlo', 'monospace']
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
